f190fbe2edc486bfe07c9f253d98fec4ed327e75
Jess Mitchell <jess@jessmitchell.ca>
2023-09-08 11:20:20 -0400

    Update samples/daily-prebuilt/permissions-can-admin/src/components/DailyContainer/DailyContainer.js
    Co-authored-by: Liza <65890040+lazeratops@users.noreply.github.com>

--- samples/daily-prebuilt/permissions-can-admin/src/components/DailyContainer/DailyContainer.js
+++ samples/daily-prebuilt/permissions-can-admin/src/components/DailyContainer/DailyContainer.js
@@ -47,7 +47,7 @@ export default function DailyContainer() {
 
   const handleParticipantUpdate = (e) => {
     console.log(e.action);
-    // // Return early if the participant list isn't set yet.
+    // Return early if the participant list isn't set yet.
     // // This event is sometimes emitted before the joined-meeting event.
     if (!prevParticipants.current[e.participant.session_id]) return;
     // Only update the participants list if the permission has changed.
